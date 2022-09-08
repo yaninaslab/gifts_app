ea3d713628d140bb844e66a939537a40cd540b72
yaninaslab <yana.belash@gmail.com>
2022-09-08 12:20:38 -0400

    Gifts app ready to go

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { data } from "./data";
 
 function App() {
+  const [gifts, setGifts] = useState(data);
+
+  const removeGift = (id) => {
+    let newGifts = gifts.filter((gift) => gift.id !== id);
+    setGifts(newGifts);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="big_container">
+      <div className="container">
+        <h1>List of {gifts.length} gifts</h1>
+      </div>
+
+      {gifts.map((element) => {
+        const { id, gift, image } = element;
+
+        return (
+          <div key={id}>
+            <div className="container">
+              <h2>
+                {id} - {gift}
+              </h2>
+            </div>
+
+            <div className="container">
+              <img src={image} width="300px" height="400px" alt="gifts" />
+            </div>
+
+            <div className="container">
+              <button onClick={() => removeGift(id)}>Remove</button>
+            </div>
+          </div>
+        );
+      })}
+      <div className="container">
+        <button onClick={() => setGifts([])}>Delete All</button>
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,55 @@
+export const data = [
+  {
+    id: 1,
+
+    gift: "Sneakers",
+
+    image:
+      "https://images.unsplash.com/photo-1595950653106-6c9ebd614d3a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fHNob2VzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+  },
+
+  {
+    id: 2,
+
+    gift: "Chocolate",
+
+    image:
+      "https://images.unsplash.com/photo-1545015451-f05567aa6bcc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80",
+  },
+
+  {
+    id: 3,
+
+    gift: "Down Coat",
+
+    image:
+      "https://cdn-tp1.mozu.com/23592-34945/cms/34945/files/812db0fd-a4d3-4ec4-96b8-185a9c57670c?maxWidth=550&_mzCb=_1661793128865",
+  },
+
+  {
+    id: 4,
+
+    gift: "AirPods",
+
+    image:
+      "https://images.unsplash.com/photo-1588423771073-b8903fbb85b5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=683&q=80",
+  },
+
+  {
+    id: 5,
+
+    gift: "iMac",
+
+    image:
+      "https://images.unsplash.com/photo-1611174340587-7cf535c00951?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80",
+  },
+
+  {
+    id: 6,
+
+    gift: "iPhone 14 Pro",
+
+    image:
+      "https://images.unsplash.com/photo-1636979865041-7d6ca1343fb4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80",
+  },
+];
